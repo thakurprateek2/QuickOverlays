08343386cf77f9e0f0b1c76a3f698db5af17ad8e
prateekt <thakurprateek2@gmail.com>
2014-01-18 19:04:25 +0530

    updating work space

--- src/com/prateek/quickoverlays/overlays/ShortcutsContainer.java
+++ src/com/prateek/quickoverlays/overlays/ShortcutsContainer.java
@@ -2,21 +2,27 @@ package com.prateek.quickoverlays.overlays;
 
 import wei.mark.standout.StandOutWindow;
 import wei.mark.standout.constants.StandOutFlags;
+import wei.mark.standout.listeners.OnSizeCahngedListner;
 import wei.mark.standout.ui.Window;
 import wei.mark.standout.ui.Window.Editor;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.widget.FrameLayout;
 
 import com.prateek.quickoverlays.R;
 
-public class ShortcutsContainer extends StandOutWindow{
-	
+public class ShortcutsContainer extends StandOutWindow {
+
 	private int oldPosX, oldPosY;
-	private int xpos = StandOutLayoutParams.RIGHT, ypos = StandOutLayoutParams.RIGHT;
+	private int xpos = StandOutLayoutParams.RIGHT,
+			ypos = StandOutLayoutParams.RIGHT;
+	private int fullFledgedWidth;
+	private int fullFledgedHeight;
+	public static final String TAG = "Overlays";
+	private boolean isStretchedFully = false;
 
 	@Override
 	public String getAppName() {
@@ -31,95 +37,94 @@ public class ShortcutsContainer extends StandOutWindow{
 	}
 	
 	@Override
+	public void onCreate() {
+		super.onCreate();
+		DisplayMetrics metrics = ShortcutsContainer.this
+				.getResources().getDisplayMetrics();
+		 fullFledgedWidth = metrics.widthPixels;
+		 fullFledgedHeight = metrics.heightPixels;
+	}
+
+	@Override
 	public boolean onTouchBody(int id, Window window, View view,
 			MotionEvent event) {
-		// TODO Auto-generated method stub
-		return super.onTouchBody(id, window, view, event);
+
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			
+			window.edit().setSize(fullFledgedWidth, fullFledgedHeight).setPosition(0, 0)
+					.commit();
+
+		} else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+			window.edit().setPosition(oldPosX, oldPosY).setSize(50, 300)
+					.commit();
+		}else if(event.getAction() == MotionEvent.ACTION_MOVE && isStretchedFully()){
+			Log.d(TAG, "co ordinates = x = " + event.getX() + " " + event.getXPrecision() + " y = " + event.getY() + " " + event.getYPrecision());
+		}
+		
+		return true;
+
+//		return super.onTouchBody(id, window, view, event);
 	}
-	
-	
 
 	@Override
 	public boolean onShow(int id, Window window) {
 		// TODO Auto-generated method stub
-		
+
 		Editor editor = window.edit();
-		
+
 		oldPosX = (int) (editor.getmParams().x + 50 * editor.getAnchorX());
 		oldPosY = (int) (editor.getmParams().y + 300 * editor.getAnchorY());
 		
-		return super.onShow(id, window);
-	}
-	
-	
-	
-	@Override
-	public void createAndAttachView(final int id, FrameLayout frame) {
-		// TODO Auto-generated method stub
-		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		View v = inflater.inflate(R.layout.shortcuts_container, frame, true);
-		v.setOnTouchListener(new OnTouchListener() {
+		window.setOnSizeCahngedListner(new OnSizeCahngedListner() {
 			
 			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				Window mWin = getWindow(id);
-				
-//				StandOutLayoutParams params = getParams(id, mWin);
-				if(event.getAction() == MotionEvent.ACTION_DOWN){
-				DisplayMetrics metrics = ShortcutsContainer.this.getResources().getDisplayMetrics();
-				int width = metrics.widthPixels;
-				int height = metrics.heightPixels;
-//				xpos = StandOutLayoutParams.CENTER;
-//				ypos = StandOutLayoutParams.CENTER;
-//				mWin.originalParams= getParams(id, mWin); 
-				
-//				params = getParams(id, mWin);
-//				mWin.touchInfo.ratio = (float) mWin.originalParams.width / mWin.originalParams.height;
-//				mWindowManager.updateViewLayout(mWin, getParams(id, mWin));
-				mWin.edit().setSize(width, height).setPosition(0, 0).commit();
-//				mWin.edit().setPosition(0, 0).commit();
-				}else if(event.getAction() == MotionEvent.ACTION_UP){
-//					xpos = StandOutLayoutParams.RIGHT;
-//					ypos = StandOutLayoutParams.RIGHT;
-//					params = getParams(id, mWin);
-//					mWin.originalParams= getParams(id, mWin); 
-//					mWin.touchInfo.ratio = (float) mWin.originalParams.width / mWin.originalParams.height;
-//					mWindowManager.updateViewLayout(mWin, getParams(id, mWin));
-					mWin.edit().setPosition(oldPosX,oldPosY).setSize(50, 300).commit();
-//					mWin.edit().setPosition(0, 0).commit();
-//					mWin.edit().setPosition(oldPosX,oldPosY).commit();
+			public void onSizeChangedListner(Window window, int w, int h, int oldw,
+					int oldh) {
+				if(w==fullFledgedWidth && h == fullFledgedHeight){
+					Log.d(TAG,"stretched fully");
+					setStretchedFully(true);
 				}
-				
-//				getWindow(id).invalidate();
-//				getFocusedWindow().edit().setSize(width, height);
-				
-				return true;
+				else{
+					setStretchedFully(false);
+				}
+//				Log.d(TAG, " h = " + h + " w = " + w + " oldh = " + oldh  + " oldw = " + oldw  + " bottom = " + window.getBottom() + " right = " + window.getRight());
 			}
 		});
-		
+
+		return super.onShow(id, window);
 	}
 
-	
-	
-	
+	@Override
+	public void createAndAttachView(final int id, FrameLayout frame) {
+		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.shortcuts_container, frame, true);
+	}
+
 	@Override
 	public StandOutLayoutParams getParams(int id, Window window) {
 		// TODO Auto-generated method stub
-		
-		
-		return new StandOutLayoutParams(id, 50, 300,
-				xpos, ypos);
-		
+
+		return new StandOutLayoutParams(id, 50, 300, xpos, ypos);
+
 	}
-	
+
 	@Override
 	public int getFlags(int id) {
 		// TODO Auto-generated method stub
-//		| StandOutFlags.FLAG_DECORATION_SYSTEM
+		// | StandOutFlags.FLAG_DECORATION_SYSTEM
+		//| StandOutFlags.FLAG_BODY_MOVE_ENABLE
 		return super.getFlags(id) 
-				| StandOutFlags.FLAG_BODY_MOVE_ENABLE
-				| StandOutFlags.FLAG_WINDOW_FOCUSABLE_DISABLE| StandOutFlags.FLAG_WINDOW_HIDE_ENABLE;
+				| StandOutFlags.FLAG_WINDOW_FOCUSABLE_DISABLE
+				| StandOutFlags.FLAG_WINDOW_HIDE_ENABLE;
+	}
+
+	public boolean isStretchedFully() {
+		return isStretchedFully;
+	}
+
+	public void setStretchedFully(boolean isStretchedFully) {
+		this.isStretchedFully = isStretchedFully;
 	}
 
 }
